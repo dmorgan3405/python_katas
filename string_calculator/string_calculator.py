50032ba1fce8fd7e21fca143827cfcf85a264323
Doug Morgan <douglas.morgan3405@gmail.com>
2016-04-26 11:31:51 -0400

    A few tests in to String Calculator

--- string_calculator/string_calculator.py
+++ string_calculator/string_calculator.py
@@ -1,4 +1,15 @@
 class StringCalculator():
-    
-    def add(self, x):
-        return 1
+
+    def __init__(self, addend_payload):
+        self.addend_payload = addend_payload.replace("\n",",")
+
+    def add(self):
+        if self.addend_payload == "": return 0
+        addends = self.map_to_addends(self.addend_payload)
+        return self.calculate_sum(addends)
+
+    def map_to_addends(self, payload):
+        return map(lambda x: int(x),payload.split(','))
+
+    def calculate_sum(self,addends):
+        return reduce(lambda x, y: x + y, addends)
--- test/test_string_calculator.py
+++ test/test_string_calculator.py
@@ -1,7 +1,17 @@
 import py.test
 from python_katas.string_calculator.string_calculator import StringCalculator
 
+def test_result_is_zero_for_empty_string():
+    assert StringCalculator("").add() == 0
+
 def test_identity():
-    calculator = StringCalculator()
-    addends = "1"
-    assert calculator.add(addends) == 1
+    assert StringCalculator("1").add() == 1
+
+def test_can_add_two_numbers():
+    assert StringCalculator("1,2").add() == 3
+
+def test_can_add_unknown_amount_of_numbers():
+    assert StringCalculator("1,2,3,4,5").add() == 15
+
+def test_can_handle_new_line_delimiter():
+    assert StringCalculator("1\n2").add() == 3
